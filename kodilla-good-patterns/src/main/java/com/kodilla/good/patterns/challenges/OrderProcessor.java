f3e660e5a3e2c763950f8f0ffb4eb13b1f5e03aa
Damian Bernat <damianbernat@wp.pl>
2022-01-09 21:41:35 +0100

    13.2 Added ProductOrderService class

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderProcessor.java
@@ -0,0 +1,28 @@
+package com.kodilla.good.patterns.challenges;
+
+public class OrderProcessor {
+    private InformationService informationService;
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+
+    public OrderProcessor(final InformationService informationService,
+                          final OrderService orderService,
+                          final OrderRepository orderRepository) {
+        this.informationService = informationService;
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+    public OrderDto process(final OrderRequest OrderRequest) {
+        boolean isOrdered = orderService.order(orderRequest.getUser(), orderRequest.getFrom(),
+                orderRequest.getTo());
+
+        if (isOrdered) {
+            informationService.inform(orderRequest.getUser());
+            orderRepository.createOrder(orderRequest.getUser(), orderRequest.getFrom(), orderRequest.getTo());
+            return new OrderDto(orderRequest.getUser(), true);
+        } else {
+            return new OrderDto(orderRequest.getUser(), false);
+        }
+    }
+}
