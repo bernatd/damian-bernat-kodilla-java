b1431241577f5c4c037e66db7f67cdd8c57c97d8
Damian Bernat <damianbernat@wp.pl>
2022-05-22 23:49:56 +0200

    31.1 Selenium Webdriver

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/config/WebDriverConfig.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing2.google.config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverConfig {
+    public final static String CHROME = "CHROME_DRIVER";
+    public static WebDriver getDriver(final String driver) {
+        System.setProperty("webdriver.chrome.driver", "/home/berny/data2/Kodilla_Kurs/chromedriver");
+        if (driver.equals(CHROME)) {
+            return new ChromeDriver();
+        } else {
+            return null;
+        }
+    }
+}
